a66438b7179fb1b271020d34479d325ce7efe1ca
Sibints2000 <sibints2000@gmail.com>
2021-08-16 22:32:23 +0530

    Seventeenth Commit

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ color_list = [(246, 240, 243), (129, 165, 205), (223, 150, 110), (31, 40, 60), (
 tim.setheading(225)
 tim.forward(250)
 tim.setheading(0)
+number_of_dots = 100
 
-for _ in range(10):
+for dot_count in range(1, number_of_dots):
     tim.dot(20, random.choice(color_list))
     tim.forward(50)
 
-    tim.setheading(90)
-    tim.forward(50)
-    tim.setheading(180)
-    tim.forward(500)
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
 
 
 
